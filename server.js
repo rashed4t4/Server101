df745a3734ea0c7e9759d18a7e04c8a81a42befc
Md Rashedul Islam <rashed4t4@gmail.com>
2018-07-25 05:03:00 +0000

    classwork 8

--- server.js
+++ server.js
@@ -3,7 +3,24 @@
   var bodyParser = require('body-parser');
   var app = express();
   var server = http.Server(app);
+  var mongo = require('mongodb');
   
+  var db, uri = "mongodb://"+process.env.IP+ ":27017";
+  
+  mongo.MongoClient.connect(uri, {useNewUrlParser:true}, function(err, client){
+    if(err){
+      console.log('Could not connect to MongoDB');
+    }
+    else{
+      db = client.db('node-cw8');
+    }
+  });
+  
+  var save = function(form_data){
+    db.createCollection('users', function(err, collection){});
+    var collection = db.collection('user');
+    collection.save(form_data);
+  }
   app.use(bodyParser.urlencoded({ extended: false }));
   app.use(bodyParser.json());
   
@@ -18,6 +35,7 @@
   });
   app.post('/submit_user', function(req, res){
     console.log(JSON.stringify(req.body));
+    save(req.body);
   });
   server.listen(process.env.PORT, process.env.IP, function(){
     console.log('Server running');
