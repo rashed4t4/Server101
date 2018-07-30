f0be0dbf5fea76d938f01af8822e16b2b7713cb3
Md Rashedul Islam <rashed4t4@gmail.com>
2018-07-30 04:30:08 +0000

    classwork 9

--- server.js
+++ server.js
@@ -3,24 +3,39 @@
   var bodyParser = require('body-parser');
   var app = express();
   var server = http.Server(app);
-  var mongo = require('mongodb');
+  var mongoose = require('mongoose');
   
-  var db, uri = "mongodb://"+process.env.IP+ ":27017";
-  
-  mongo.MongoClient.connect(uri, {useNewUrlParser:true}, function(err, client){
-    if(err){
-      console.log('Could not connect to MongoDB');
-    }
-    else{
-      db = client.db('node-cw8');
-    }
+  mongoose.connect( "mongodb://localhost:27017/node-cw9");
+  mongoose.connection.on('error', function(){
+    console.log('could not connect to mongodb')
   });
   
-  var save = function(form_data){
-    db.createCollection('users', function(err, collection){});
-    var collection = db.collection('user');
-    collection.save(form_data);
-  }
+  var userSchema = new mongoose.Schema({
+    name: {
+      type: String,
+      required: "Name is required"
+    },
+    email: String
+  })
+  
+  var User = mongoose.model('User', userSchema);
+  
+  // var db, uri = "mongodb://"+process.env.IP+ ":27017";
+  
+  // mongo.MongoClient.connect(uri, {useNewUrlParser:true}, function(err, client){
+  //   if(err){
+  //     console.log('Could not connect to MongoDB');
+  //   }
+  //   else{
+  //     db = client.db('node-cw8');
+  //   }
+  // });
+  
+  // var save = function(form_data){
+  //   db.createCollection('users', function(err, collection){});
+  //   var collection = db.collection('users');
+  //   collection.save(form_data);
+  // }
   app.use(bodyParser.urlencoded({ extended: false }));
   app.use(bodyParser.json());
   
@@ -34,8 +49,15 @@
     res.sendFile(__dirname+'/form.html');
   });
   app.post('/submit_user', function(req, res){
-    console.log(JSON.stringify(req.body));
-    save(req.body);
+    console.log(req.body);
+    var new_user = new User(req.body);
+    new_user.save(function(err,data){
+      if(err){
+        return res.status(400).json({message:"Couldn't save user"})
+      }
+      res.status(200).json(data);
+    })
+    
   });
   server.listen(process.env.PORT, process.env.IP, function(){
     console.log('Server running');
